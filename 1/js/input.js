bd3ba229c9ab6890408671eb1ff81ea2830bce9b
temporal-ripple <3052573616@qq.com>
2026-05-18 22:16:49 +0800

    feat: add input handling system

---
+++ 1/js/input.js
@@ -0,0 +1,56 @@
+class Input {
+    constructor() {
+        this.keys = {};
+        this.justPressed = {};
+        this.mouse = { x: 0, y: 0, clicked: false };
+        this.setupListeners();
+    }
+
+    setupListeners() {
+        window.addEventListener('keydown', (e) => {
+            if (!this.keys[e.code]) {
+                this.justPressed[e.code] = true;
+            }
+            this.keys[e.code] = true;
+        });
+
+        window.addEventListener('keyup', (e) => {
+            this.keys[e.code] = false;
+        });
+
+        const canvas = document.getElementById('game-canvas');
+        canvas.addEventListener('mousemove', (e) => {
+            const rect = canvas.getBoundingClientRect();
+            this.mouse.x = e.clientX - rect.left;
+            this.mouse.y = e.clientY - rect.top;
+        });
+
+        canvas.addEventListener('click', (e) => {
+            this.mouse.clicked = true;
+        });
+    }
+
+    isKeyDown(code) {
+        return this.keys[code] || false;
+    }
+
+    isKeyJustPressed(code) {
+        return this.justPressed[code] || false;
+    }
+
+    clearJustPressed() {
+        this.justPressed = {};
+        this.mouse.clicked = false;
+    }
+
+    getDirection() {
+        let dx = 0, dy = 0;
+        if (this.isKeyDown('KeyW') || this.isKeyDown('ArrowUp')) dy = -1;
+        if (this.isKeyDown('KeyS') || this.isKeyDown('ArrowDown')) dy = 1;
+        if (this.isKeyDown('KeyA') || this.isKeyDown('ArrowLeft')) dx = -1;
+        if (this.isKeyDown('KeyD') || this.isKeyDown('ArrowRight')) dx = 1;
+        return { dx, dy };
+    }
+}
+
+const input = new Input();
